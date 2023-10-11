fabed9ba41807d7602f4c30e2e34c9df23df9528
Your Name <youremail@example.com>
2023-10-11 22:00:23 +0000

    current

--- app.js
+++ app.js
@@ -1,148 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dashboard = [
-    {
-      title: "Work",
-      timeframes: {
-        daily: {
-          current: 5,
-          previous: 7,
-        },
-        weekly: {
-          current: 32,
-          previous: 36,
-        },
-        monthly: {
-          current: 103,
-          previous: 128,
-        },
-      },
-    },
-    {
-      title: "Play",
-      timeframes: {
-        daily: {
-          current: 1,
-          previous: 2,
-        },
-        weekly: {
-          current: 10,
-          previous: 8,
-        },
-        monthly: {
-          current: 23,
-          previous: 29,
-        },
-      },
-    },
-    {
-      title: "Study",
-      timeframes: {
-        daily: {
-          current: 0,
-          previous: 1,
-        },
-        weekly: {
-          current: 4,
-          previous: 7,
-        },
-        monthly: {
-          current: 13,
-          previous: 19,
-        },
-      },
-    },
-    {
-      title: "Exercise",
-      timeframes: {
-        daily: {
-          current: 1,
-          previous: 1,
-        },
-        weekly: {
-          current: 4,
-          previous: 5,
-        },
-        monthly: {
-          current: 11,
-          previous: 18,
-        },
-      },
-    },
-    {
-      title: "Social",
-      timeframes: {
-        daily: {
-          current: 1,
-          previous: 3,
-        },
-        weekly: {
-          current: 5,
-          previous: 10,
-        },
-        monthly: {
-          current: 21,
-          previous: 23,
-        },
-      },
-    },
-    {
-      title: "Self Care",
-      timeframes: {
-        daily: {
-          current: 0,
-          previous: 1,
-        },
-        weekly: {
-          current: 2,
-          previous: 2,
-        },
-        monthly: {
-          current: 7,
-          previous: 11,
-        },
-      },
-    },
-  ];
-
-  // Function to update all titles with class "inbox1" based on the selected timeframe
-  function updateTitles(timeframe) {
-    const boxes = document.querySelectorAll(".inbox1");
-    boxes.forEach((box, index) => {
-      const sectionData = dashboard[index];
-      const titleElement = box.querySelector("#title");
-
-      // Clear existing content and add the title with the ellipsis icon
-      titleElement.innerHTML = `
-          ${sectionData.title} <i class="fas fa-ellipsis"></i>
-        `;
-
-      const hourElement = box.querySelector("#hour");
-      const statusElement = box.querySelector("#status");
-
-      hourElement.textContent = `${sectionData.timeframes[timeframe].current}hrs`;
-      statusElement.textContent = `Last ${
-        timeframe.charAt(0).toUpperCase() + timeframe.slice(1)
-      }- ${sectionData.timeframes[timeframe].previous}hrs`;
-    });
+  function updateData(timeframe) {
+    fetch("data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((activity, index) => {
+          const current = activity.timeframes[timeframe].current;
+          const previous = activity.timeframes[timeframe].previous;
+          const box = document.querySelector(`#activity-${index}`);
+          box.querySelector("#hour").textContent = `${current}hrs`;
+          box.querySelector(
+            "#status"
+          ).textContent = `Last Week- ${previous}hrs`;
+        });
+      });
   }
 
-  // Add event listeners to buttons
-  const dailyButton = document.getElementById("daily-button");
-  const weeklyButton = document.getElementById("weekly-button");
-  const monthlyButton = document.getElementById("monthly-button");
+  document
+    .getElementById("daily-button")
+    .addEventListener("click", function () {
+      updateData("daily");
+    });
 
-  dailyButton.addEventListener("click", () => {
-    updateTitles("daily");
-  });
+  document
+    .getElementById("weekly-button")
+    .addEventListener("click", function () {
+      updateData("weekly");
+    });
 
-  weeklyButton.addEventListener("click", () => {
-    updateTitles("weekly");
-  });
+  document
+    .getElementById("monthly-button")
+    .addEventListener("click", function () {
+      updateData("monthly");
+    });
 
-  monthlyButton.addEventListener("click", () => {
-    updateTitles("monthly");
-  });
-
-  // Initialize the section (you can choose a default or leave it empty)
-  updateTitles("weekly");
+  // Load initial data (default: "Weekly")
+  updateData("weekly");
 });
